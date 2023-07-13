7dbc1dc192c96d131a83ece2f736be720a7d5783
ncpa0cpl <sbretner@touchbistro.com>
2023-07-13 10:44:17 +0200

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export { CopiesList as CopyList } from "./entity-containers/copy-list";
-export { Entity as EntitySingleton } from "./entity-containers/entity";
+export { CopiesList } from "./entity-containers/copy-list";
+export { Entity } from "./entity-containers/entity";
 export { EntityList } from "./entity-containers/entity-list";
-export { BaseEntity as Entity } from "./entity/base-entity";
+export { BaseEntity } from "./entity/base-entity";
 export { Ledger } from "./ledger/ledger";
 export type {
   Copy,
